860fde18c5f0668febf49e35afd422254d2d94fb
Kenneth Phang <bunnyppl@gmail.com>
2016-11-12 00:36:33 +0800

    new codedeploy

--- bin/start.sh
+++ bin/start.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 export AWS_ACCESS_KEY_ID=AKIAIWKXTGYCOTZW6QYA
 export AWS_SECRET_ACCESS_KEY=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
-nohup nodemon ../server/app.js > ../logs/output.log &
+forever start ../server/app.js > ../logs/output.log &
