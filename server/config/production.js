6ad683d423513ac14abbcfc6a35088afe68505b8
Kenneth Phang <bunnyppl@gmail.com>
2016-11-22 01:02:07 +0800

    make changes to the social logins

--- server/config/production.js
+++ server/config/production.js
@@ -1,10 +1,10 @@
 'use strict';
 
 module.exports = {
-    mysql: process.env.CLEARDB_DATABASE_URL,
+    mysql: process.env.MYSQL_DATABASE_URL,
     aws: {
-        id: "AKIAIJ3DEOUG6JTZNBRA",
-        key: "qC8TkHF4Tbsr8mDustQ/mykGDCh6pEwUUtu2J+jf",
+        id: "AKIAIWKXTGYCOTZW6QYA",
+        key: "xFOWIuR0hMbQvsodFa5VenHWwnNf3fDibyjMpZIb",
         url: "https://wedding-gram.s3.amazonaws.com",
         bucket: "wedding-gram"
     },
@@ -12,18 +12,18 @@ module.exports = {
     seed: process.env.SEED || false,
     linkedin_key: "81xv9l7ppiocvz",
     Linkedin_secret: "LIhcEze930Dx5RmZ",
-    Linkedin_callback_url: "http://localhost:3000/oauth/linkedin/callback",
+    Linkedin_callback_url: "http://ec2-54-254-206-254.ap-southeast-1.compute.amazonaws.com/oauth/linkedin/callback",
     GooglePlus_key: "552301539640-morchf1e4ig6q7gtfje4fl7l35i99uiv.apps.googleusercontent.com",
     GooglePlus_secret: "JbU_KW4qhXGv8eS7_j8p7f-t",
-    GooglePlus_callback_url: "http://localhost:3000/oauth/google/callback",
+    GooglePlus_callback_url: "http://ec2-54-254-206-254.ap-southeast-1.compute.amazonaws.com/oauth/google/callback",
     Facebook_key: "1774952232780596",
     Facebook_secret: "bbd0cddf9a4605175f4d6fdfc951491e",
-    Facebook_callback_url: "http://localhost:3000/oauth/facebook/callback",
+    Facebook_callback_url: "http://ec2-54-254-206-254.ap-southeast-1.compute.amazonaws.com/oauth/facebook/callback",
     Twitter_key: "IsK3ZwAeez0hMD2Eo7GVjz8sm",
     Twitter_secret: "fiB5McI42sQxb6Q8aOYnlW2n2xSt5cgnLiVNcAlYyT7xCRhZXd",
-    Twitter_callback_url: "http://localhost:3000/oauth/twitter/callback",
+    Twitter_callback_url: "http://ec2-54-254-206-254.ap-southeast-1.compute.amazonaws.com/oauth/twitter/callback",
     Wechat_AppId: "1",
     Wechat_Name: "1",
     Wechat_AppSecret: "1",
-    Wechat_Callback_Url: "http://localhost:3000/oauth/wechat/callback"
+    Wechat_Callback_Url: "http://ec2-54-254-206-254.ap-southeast-1.compute.amazonaws.com/oauth/wechat/callback"
 };
